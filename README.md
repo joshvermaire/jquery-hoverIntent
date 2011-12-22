41d6d234ddd0a4126bd054155eafe55eda8255c9
Josh Vermaire <joshvermaire@gmail.com>
2011-12-22 01:09:35 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 hoverIntent jQuery Plug-in
 ==========================
+Refactored hoverIntent jQuery Plugin-in
+
+I will be changing hoverIntent to take advantage of some of the more sophisticated, recent abilities of jQuery. This will include binding, multiple or single callbacks, and delegation.
+
+If you're interested in helping with thoughts or ideas, please let me know.
+
+Josh Vermaire
+
+
+Original hoverIntent jQuery Plugin-in
 
 hoverIntent is a plug-in that attempts to determine the user's intent... like a crystal ball, only with mouse movement! It works like (and was derived from) [jQuery](http://jquery.com/)'s built-in [hover](http://api.jquery.com/hover/). However, instead of immediately calling the onMouseOver function, it waits until the user's mouse slows down enough before making the call.
 
--- jquery.hoverIntent.js
+++ jquery.hoverIntent.js
@@ -1,106 +1,83 @@
 ﻿/**
-* hoverIntent is similar to jQuery's built-in "hover" function except that
-* instead of firing the onMouseOver event immediately, hoverIntent checks
-* to see if the user's mouse has slowed down (beneath the sensitivity
-* threshold) before firing the onMouseOver event.
-* 
-* hoverIntent r6 // 2011.02.26 // jQuery 1.5.1+
+* This fork of hoverIntent is building on Brian Cherne's original work.
+* Find out more about his work at:
 * <http://cherne.net/brian/resources/jquery.hoverIntent.html>
 * 
 * hoverIntent is currently available for use in all personal or commercial 
 * projects under both MIT and GPL licenses. This means that you can choose 
 * the license that best suits your project, and use it accordingly.
 * 
-* // basic usage (just like .hover) receives onMouseOver and onMouseOut functions
-* $("ul li").hoverIntent( showNav , hideNav );
+* current usage only takes one callback into consideration. In order to 
+* create a method that takes into consideration the previous onMouseOver
+* and onMouseOut functions, use event.originalEvent.type in the callback
+* and listen for mouseover and mouseout events.
 * 
-* // advanced usage receives configuration object only
-* $("ul li").hoverIntent({
-*	sensitivity: 7, // number = sensitivity threshold (must be 1 or higher)
-*	interval: 100,   // number = milliseconds of polling interval
-*	over: showNav,  // function = onMouseOver callback (required)
-*	timeout: 0,   // number = milliseconds delay before onMouseOut function call
-*	out: hideNav    // function = onMouseOut callback (required)
-* });
+* $("ul li").bind('hoverIntent', callback);
+* You can extend the default configuration by modifying the object
+* $.event.special.hoverIntent.
 * 
-* @param  f  onMouseOver function || An object with configuration options
-* @param  g  onMouseOut function  || Nothing (use configuration options object)
-* @author    Brian Cherne brian(at)cherne(dot)net
 */
-(function($) {
-	$.fn.hoverIntent = function(f,g) {
-		// default configuration options
-		var cfg = {
-			sensitivity: 7,
-			interval: 100,
-			timeout: 0
-		};
-		// override configuration options with user supplied object
-		cfg = $.extend(cfg, g ? { over: f, out: g } : f );
-
-		// instantiate variables
-		// cX, cY = current X and Y position of mouse, updated by mousemove event
-		// pX, pY = previous X and Y position of mouse, set by mouseover and polling interval
-		var cX, cY, pX, pY;
-
-		// A private function for getting mouse position
-		var track = function(ev) {
-			cX = ev.pageX;
-			cY = ev.pageY;
-		};
-
-		// A private function for comparing current and previous mouse position
-		var compare = function(ev,ob) {
-			ob.hoverIntent_t = clearTimeout(ob.hoverIntent_t);
-			// compare mouse positions to see if they've crossed the threshold
-			if ( ( Math.abs(pX-cX) + Math.abs(pY-cY) ) < cfg.sensitivity ) {
-				$(ob).unbind("mousemove",track);
-				// set hoverIntent state to true (so mouseOut can be called)
-				ob.hoverIntent_s = 1;
-				return cfg.over.apply(ob,[ev]);
-			} else {
-				// set previous coordinates for next time
-				pX = cX; pY = cY;
-				// use self-calling timeout, guarantees intervals are spaced out properly (avoids JavaScript timer bugs)
-				ob.hoverIntent_t = setTimeout( function(){compare(ev, ob);} , cfg.interval );
-			}
-		};
-
-		// A private function for delaying the mouseOut function
-		var delay = function(ev,ob) {
-			ob.hoverIntent_t = clearTimeout(ob.hoverIntent_t);
-			ob.hoverIntent_s = 0;
-			return cfg.out.apply(ob,[ev]);
-		};
-
-		// A private function for handling mouse 'hovering'
-		var handleHover = function(e) {
-			// copy objects to be passed into t (required for event object to be passed in IE)
-			var ev = jQuery.extend({},e);
-			var ob = this;
-
-			// cancel hoverIntent timer if it exists
-			if (ob.hoverIntent_t) { ob.hoverIntent_t = clearTimeout(ob.hoverIntent_t); }
-
-			// if e.type == "mouseenter"
-			if (e.type == "mouseenter") {
-				// set "previous" X and Y position based on initial entry point
-				pX = ev.pageX; pY = ev.pageY;
-				// update "current" X and Y position based on mousemove
-				$(ob).bind("mousemove",track);
-				// start polling interval (self-calling timeout) to compare mouse coordinates over time
-				if (ob.hoverIntent_s != 1) { ob.hoverIntent_t = setTimeout( function(){compare(ev,ob);} , cfg.interval );}
-
-			// else e.type == "mouseleave"
-			} else {
-				// unbind expensive mousemove event
-				$(ob).unbind("mousemove",track);
-				// if hoverIntent state is true, then call the mouseOut function after the specified delay
-				if (ob.hoverIntent_s == 1) { ob.hoverIntent_t = setTimeout( function(){delay(ev,ob);} , cfg.timeout );}
-			}
-		};
-
-		// bind the function to the two event listeners
-		return this.bind('mouseenter',handleHover).bind('mouseleave',handleHover);
-	};
-})(jQuery);
+$(document).ready(function() {
+      var cX, cY, pX, pY;
+      return $.event.special.hoverIntent = {
+        cfg: {
+          sensitivity: 7,
+          interval: 100,
+          timeout: 0
+        },
+        setup: function() {
+          var handler;
+          handler = function(event) {
+            var applyCallback, args, cfg, compare, delay, ob, track;
+            ob = this;
+            cfg = jQuery.event.special.hoverIntent.cfg;
+            args = arguments;
+            applyCallback = function() {
+              event.type = 'hoverIntent';
+              return jQuery.event.handle.apply(ob, args);
+            };
+            delay = function() {
+              ob.hoverIntent_t = clearTimeout(ob.hoverIntent_t);
+              ob.hoverIntent_s = 0;
+              return applyCallback();
+            };
+            track = function(event) {
+              cX = event.pageX;
+              return cY = event.pageY;
+            };
+            compare = function() {
+              ob.hoverIntent_t = clearTimeout(ob.hoverIntent_t);
+              if ((Math.abs(pX - cX) + Math.abs(pY - cY)) < cfg.sensitivity) {
+                $(ob).unbind("mousemove", track);
+                ob.hoverIntent_s = 1;
+                return applyCallback();
+              } else {
+                pX = cX;
+                pY = cY;
+                return ob.hoverIntent_t = setTimeout(compare, cfg.interval);
+              }
+            };
+            if (ob.hoverIntent_t) {
+              ob.hoverIntent_t = clearTimeout(ob.hoverIntent_t);
+            }
+            if (event.type === "mouseenter") {
+              pX = event.pageX;
+              pY = event.pageY;
+              $(ob).bind("mousemove", track);
+              if (ob.hoverIntent_s !== 1) {
+                return ob.hoverIntent_t = setTimeout(compare, cfg.interval);
+              }
+            } else {
+              $(ob).unbind("mousemove", track);
+              if (ob.hoverIntent_s === 1) {
+                return ob.hoverIntent_t = setTimeout(delay, cfg.timeout);
+              }
+            }
+          };
+          return $(this).bind('mouseenter', handler).bind("mouseleave", handler);
+        },
+        teardown: function(namespaces) {
+          return $(this).unbind('mouseenter', $(this)).unbind("mouseleave", $(this));
+        }
+      };
+    });
